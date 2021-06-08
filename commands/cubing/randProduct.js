0860289c48440eb830d8f0de3829910a1b398203
Lukas Batema <lukasbatema@gmail.com>
2021-06-08 09:33:21 -0400

    test

--- commands/cubing/randProduct.js
+++ commands/cubing/randProduct.js
@@ -1,4 +1,4 @@
-const { Command } = require('discord.js-commando');
+const { Command } = require("discord.js-commando");
 const botData = require("../../botData.js");
 const discord = require("discord.js");
 const StormDB = require("stormdb");
